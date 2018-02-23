b31fc00db12a59ea9b4246c48ff159d0c9cf3706
CT Li <33132596+ChentongLi@users.noreply.github.com>
2018-02-22 21:44:44 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <cstring>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <climits>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <list>
+#include <time.h>
 
-using name space std;
+using namespace std;
+
+
+int main(int argc, char *argv[]){
+
+        init();
+}
