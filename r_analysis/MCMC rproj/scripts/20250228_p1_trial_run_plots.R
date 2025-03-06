f1556d35f3861069eca83d5ad7384a31953adb66
alliekreitman <allie.kreitman@gmail.com>
2025-03-06 11:12:33 -0800

    add plots and ACF calc

--- r_analysis/MCMC rproj/scripts/20250228_p1_trial_run_plots.R
+++ r_analysis/MCMC rproj/scripts/20250228_p1_trial_run_plots.R
@@ -1,5 +1,6 @@
 #load libraries
 library(tidyverse)
+library(cowplot)
 
 # load data
 # Read the file
@@ -19,7 +20,52 @@ mu_results <- tibble(
   Nbegin = as.numeric(str_extract(mu_lines[seq(1, length(mu_lines), by = 3)], "(?<=\\s)[0-9\\.]+$")),
   Unique_Tree_count = as.numeric(gsub("Unique_Tree_count=", "", mu_lines[seq(2, length(mu_lines), by = 3)])),
   Iter_count = as.numeric(gsub("Iter_count=", "", mu_lines[seq(3, length(mu_lines), by = 3)]))
-)
+) %>% 
+  mutate(log_Nbegin = log(Nbegin))
+
+###### traceplots
+omega1_traceplot <- ggplot(omega_results, aes(x = Iter_count, y = omega1))+
+  theme_bw()+
+  geom_point()
+
+omega2_traceplot <- ggplot(omega_results, aes(x = Iter_count, y = omega2))+
+  theme_bw()+
+  geom_point()
+
+lambda_traceplot <- ggplot(mu_results, aes(x = Iter_count, y = lambda))+
+  theme_bw()+
+  geom_point()
+
+log_Nbegin_traceplot <- ggplot(mu_results, aes(x = Iter_count, y = log_Nbegin))+
+  theme_bw()+
+  geom_point()
+  # scale_y_continuous(limits = c(0, max(mu_results$Nbegin)))  # Set min/max for y-axis
+
+traceplots <- cowplot::plot_grid(omega1_traceplot, omega2_traceplot, lambda_traceplot, log_Nbegin_traceplot)
+traceplots
+
+# plot the ACFs (also 4 subplots as above) 
+
+# Compute ACF using dplyr
+acf_data <- full_join(omega_results, mu_results, by = "Iter_count", suffix = c(".omega", ".mu")) %>%
+  select(omega1, omega2, lambda, log_Nbegin) %>%
+  pivot_longer(everything(), names_to = "parameter", values_to = "value") %>%
+  group_by(parameter) %>%
+  summarise(acf_values = list(acf(value, plot = FALSE)$acf),
+            lags = list(acf(value, plot = FALSE)$lag),
+            .groups = "drop") %>%
+  unnest(c(acf_values, lags))
+
+# Plot the ACFs
+ggplot(acf_data, aes(x = lags, y = acf_values)) +
+  theme_bw()+
+  geom_line() +
+  geom_point() +
+  facet_wrap(~parameter, scales = "free_y") +
+  labs(title = "Autocorrelation Function (ACF) for Parameters",
+       x = "Lag",
+       y = "Autocorrelation") 
+
 
 # combine dataframes 
 omega_mu_results <- rbind(
@@ -31,6 +77,16 @@ omega_mu_results <- rbind(
 
 # plot the entire chain trace plot -- i.e. make one figure with 4 subplots; 
   # one subplot for each of the 4 parameters (omega1, omega2, lambda, Nbegin)
-
+ggplot(data = head(omega_mu_results, n = 33000)) +
+  geom_point(aes(x = Iter_count, y = value))+
+  theme_bw()+
+  facet_grid(~variable)
+  
+  
+ggplot(data = omega_mu_results)+
+  theme_bw()+
+  geom_histogram(aes(x = value))+
+  facet_grid(~variable)
 
 # plot the ACFs (also 4 subplots as above) 
+
