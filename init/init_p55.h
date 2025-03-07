8795963677cf26b4b65816460f65aac15d7cf59c
alliekreitman <allie.kreitman@gmail.com>
2025-03-06 18:56:32 -0800

    create init files

---
+++ init/init_p55.h
@@ -0,0 +1,137 @@
+int IsInclude(char *a,char b[20][10],int n){
+
+    int i;
+    for (i=0;i<n;i++)
+        if (strcmp(a,b[i])==0)
+            return 1;
+
+    return 0;
+
+}
+int checkseq(char *seqen,int n){
+    int i;
+    char *tmp=calloc(3,sizeof(char));
+    for(i=0;i<n;i+=3){
+        strncpy(tmp,seqen+i,3);
+        if (strcmp(tmp,"TAA")==0 || strcmp(tmp,"TGA")==0 || strcmp(tmp,"TAG")==0)
+            return 0;
+    }
+    free(tmp);
+    return 1;
+}
+
+// change these 3 lines for each patient
+#define DATADAYS 8
+double datatime[DATADAYS+1]={-1,0,7,112,140,224,504,672,756};
+int datanum[DATADAYS+1]={0,22,6,3,8,7,8,7,5};
+
+BiNode *init()
+{
+
+    FASTAFILE *ffp;
+    char *seq;
+    char *name;
+    int   L;
+    char *filename="fasta/in_publication/p55sequence.fasta";  // change for each patient
+
+	int n=0;
+	BiNode* S[MAXDATASIZE];
+	int top=-1;
+	ffp = OpenFASTA(filename);
+
+    if (ffp==NULL) {
+
+        printf("wrong input!\n");
+        return NULL;
+    }
+
+
+	// data name - change for each patient
+	char b0[][10]={"AY000759","AY000761","AY000763","AY000766","AY000767","AY000768","AY000770","AY000761","AY000778","AY000791","AY000792","AY000793","AY000794","AY000796","AY000798","AY000761","AY000809","AY000811","AY000812","AY000813","AY000814","AY000815"};
+    	char b1[][10]={"AY000769","AY000772","AY000773","AY000807","AY000808","AY000816"};
+	char b2[][10]={"AY000771","AY000800","AY000803"};
+	char b3[][10]={"AY000784","AY000785","AY000795","AY000797","AY000799","AY000801","AY000802","AY000817"};
+  	char b4[][10]={"AY000757","AY000758","AY000760","AY000762","AY000764","AY000765","AY000820"};
+   	char b5[][10]={"AY000781","AY000786","AY000787","AY000789","AY000790","AY000806","AY000818","AY000819"};
+   	char b6[][10]={"AY000776","AY000777","AY000780","AY000783","AY000788","AY000804","AY000805"};
+    	char b7[][10]={"AY000774","AY000775","AY000779","AY000782","AY000810"};
+
+
+
+	while(ReadFASTA(ffp, &seq, &name, &L)){
+        S[n]=calloc(1,sizeof(BiNode));
+		S[n]->lchi=NULL;
+		S[n]->rchi=NULL;
+		S[n]->parent=NULL;
+        S[n]->sequence=calloc(1000,sizeof(char));
+        S[n]->name=calloc(10,sizeof(char));
+	    strcpy(S[n]->sequence,seq);
+	    strncpy(S[n]->name,name,8);
+
+        //
+	    if (IsInclude(S[n]->name,b0,datanum[1])) {S[n]->NodeTime=datatime[1];}
+	    else if (IsInclude(S[n]->name,b1,datanum[2])) {S[n]->NodeTime=datatime[2];}
+	    else if (IsInclude(S[n]->name,b2,datanum[3])) {S[n]->NodeTime=datatime[3];}
+	    else if (IsInclude(S[n]->name,b3,datanum[4])) {S[n]->NodeTime=datatime[4];}
+	    else if (IsInclude(S[n]->name,b4,datanum[5])) {S[n]->NodeTime=datatime[5];}
+	    else if (IsInclude(S[n]->name,b5,datanum[6])) {S[n]->NodeTime=datatime[6];}
+	    else if (IsInclude(S[n]->name,b6,datanum[7])) {S[n]->NodeTime=datatime[7];}
+	    else if (IsInclude(S[n]->name,b7,datanum[8])) {S[n]->NodeTime=datatime[8];}
+	    //else if (IsInclude(S[n]->name,b8,datanum[9])) {S[n]->NodeTime=datatime[9];}
+	    else {
+                printf("%s have no time\n",S[n]->name);
+                S[n]->NodeTime=252;
+	    }
+	    free(name);
+	    free(seq);
+        
+        if(checkseq(S[n]->sequence,strlen(S[n]->sequence))) n++;
+        else{
+            free(S[n]->sequence);
+            free(S[n]->name);
+            free(S[n]);
+        }
+	}
+	seqlength=strlen(S[1]->sequence);
+	//printf("number of node=%d\n",n);
+	top=n-1;
+	//printf("length %d\n",strlen(S[top]->sequence));
+	int i,j,len=n;
+	int judge[MAXDATASIZE];
+	//for (i=0;i<len;i++) printf("S[%d]=%d\n",i,S[i]->data);
+
+	for (i=0;i<MAXDATASIZE;i++) judge[i]=i;
+	//srand(time(0));
+	int r;
+	while(len>1){ //shuffle algorithm to build the Tree
+		top++;
+		S[top]=calloc(1,sizeof(BiNode));
+		S[top]->name=calloc(10,sizeof(char));
+		strcpy(S[top]->name,"AYNULL");
+		S[top]->sequence=NULL;
+		r=rand()%len;
+		j=judge[r];
+		S[top]->lchi=S[j];
+        S[j]->parent=S[top];
+		len--;
+		judge[r]=judge[len];
+		//printf("j=%d\n",j);
+
+		r=rand()%len;
+		j=judge[r];
+		S[top]->rchi=S[j];
+		S[j]->parent=S[top];
+		len--;
+		judge[r]=judge[len];
+		judge[len]=top;
+		len++;
+
+		S[top]->NodeTime=MIN(S[top]->lchi->NodeTime,S[top]->rchi->NodeTime)-1;
+		S[top]->parent=NULL;
+		//printf("j=%d\n\n",j);
+
+	}
+    //printf("top=%d\n",top);
+	//for (i=0;i<top+1;i++) printf("S[%d]=%d\n",i,S[i]->data);
+	return S[top];
+}
---
+++ init/init_p60.h
@@ -0,0 +1,134 @@
+int IsInclude(char *a,char b[20][10],int n){
+
+    int i;
+    for (i=0;i<n;i++)
+        if (strcmp(a,b[i])==0)
+            return 1;
+
+    return 0;
+
+}
+int checkseq(char *seqen,int n){
+    int i;
+    char *tmp=calloc(3,sizeof(char));
+    for(i=0;i<n;i+=3){
+        strncpy(tmp,seqen+i,3);
+        if (strcmp(tmp,"TAA")==0 || strcmp(tmp,"TGA")==0 || strcmp(tmp,"TAG")==0)
+            return 0;
+    }
+    free(tmp);
+    return 1;
+}
+
+// change these 3 lines for each patient
+#define DATADAYS 6 //defined as number of days with data
+double datatime[DATADAYS+1]={-1,0,56,140,336,627,672};
+int datanum[DATADAYS+1]={0,15,7,6,5,5,6};
+
+BiNode *init()
+{
+
+    FASTAFILE *ffp;
+    char *seq;
+    char *name;
+    int   L;
+    char *filename="fasta/in_publication/p60sequence.fasta";  // change for each patient
+
+	int n=0;
+	BiNode* S[MAXDATASIZE];
+	int top=-1;
+	ffp = OpenFASTA(filename);
+
+    if (ffp==NULL) {
+
+        printf("wrong input!\n");
+        return NULL;
+    }
+
+
+	// data name - change for each patient
+	char b0[][10]={"AY001157","AY001158","AY001161","AY001164","AY001170","AY001171","AY001172","AY001181","AY001189","AY001192","AY001197","AY001200","AY001202","AY001160","AY001203"};
+    	char b1[][10]={"AY001166","AY001167","AY001168","AY001169","AY001176","AY001191","AY001191"};
+	char b2[][10]={"AY001159","AY001162","AY001163","AY001187","AY001198","AY001204"};
+	char b3[][10]={"AY001165","AY001177","AY001185","AY001186","AY001188"};
+  	char b4[][10]={"AY001173","AY001174","AY001178","AY001179","AY001193"};
+   	char b5[][10]={"AY001175","AY001180","AY001182","AY001183","AY001184","AY001195"};
+
+
+
+
+	while(ReadFASTA(ffp, &seq, &name, &L)){
+        S[n]=calloc(1,sizeof(BiNode));
+		S[n]->lchi=NULL;
+		S[n]->rchi=NULL;
+		S[n]->parent=NULL;
+        S[n]->sequence=calloc(1000,sizeof(char));
+        S[n]->name=calloc(10,sizeof(char));
+	    strcpy(S[n]->sequence,seq);
+	    strncpy(S[n]->name,name,8);
+
+        //
+	    if (IsInclude(S[n]->name,b0,datanum[1])) {S[n]->NodeTime=datatime[1];}
+	    else if (IsInclude(S[n]->name,b1,datanum[2])) {S[n]->NodeTime=datatime[2];}
+	    else if (IsInclude(S[n]->name,b2,datanum[3])) {S[n]->NodeTime=datatime[3];}
+	    else if (IsInclude(S[n]->name,b3,datanum[4])) {S[n]->NodeTime=datatime[4];}
+	    else if (IsInclude(S[n]->name,b4,datanum[5])) {S[n]->NodeTime=datatime[5];}
+	    else if (IsInclude(S[n]->name,b5,datanum[6])) {S[n]->NodeTime=datatime[6];}
+	    //else if (IsInclude(S[n]->name,b6,datanum[7])) {S[n]->NodeTime=datatime[7];}
+	    else {
+                printf("%s have no time\n",S[n]->name);
+                S[n]->NodeTime=252;
+	    }
+	    free(name);
+	    free(seq);
+        
+        if(checkseq(S[n]->sequence,strlen(S[n]->sequence))) n++;
+        else{
+            free(S[n]->sequence);
+            free(S[n]->name);
+            free(S[n]);
+        }
+	}
+	seqlength=strlen(S[1]->sequence);
+	//printf("number of node=%d\n",n);
+	top=n-1;
+	//printf("length %d\n",strlen(S[top]->sequence));
+	int i,j,len=n;
+	int judge[MAXDATASIZE];
+	//for (i=0;i<len;i++) printf("S[%d]=%d\n",i,S[i]->data);
+
+	for (i=0;i<MAXDATASIZE;i++) judge[i]=i;
+	//srand(time(0));
+	int r;
+	while(len>1){ //shuffle algorithm to build the Tree
+		top++;
+		S[top]=calloc(1,sizeof(BiNode));
+		S[top]->name=calloc(10,sizeof(char));
+		strcpy(S[top]->name,"AYNULL");
+		S[top]->sequence=NULL;
+		r=rand()%len;
+		j=judge[r];
+		S[top]->lchi=S[j];
+        S[j]->parent=S[top];
+		len--;
+		judge[r]=judge[len];
+		//printf("j=%d\n",j);
+
+		r=rand()%len;
+		j=judge[r];
+		S[top]->rchi=S[j];
+		S[j]->parent=S[top];
+		len--;
+		judge[r]=judge[len];
+		judge[len]=top;
+		len++;
+
+		S[top]->NodeTime=MIN(S[top]->lchi->NodeTime,S[top]->rchi->NodeTime)-1;
+		S[top]->parent=NULL;
+		//printf("j=%d\n\n",j);
+
+	}
+    //printf("top=%d\n",top);
+	//for (i=0;i<top+1;i++) printf("S[%d]=%d\n",i,S[i]->data);
+	return S[top];
+}
